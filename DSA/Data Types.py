d1ae8b79322bf4815c2cb055b7e6774bf2349a8b
Kenean Dita <KeneanSufa@gmail.com>
2025-03-22 20:04:31 +0000

    Basic DSA in python

---
+++ DSA/Data Types.py
@@ -0,0 +1,15 @@
+#basic data types
+str = "String"
+print(str[3])
+
+lis =  [1, "hi there", 6 ]
+print(lis[1])
+
+tupe = ("kenean", 56, [1, 2, "Atinaf"])
+print(tupe[2])
+
+dict = {12:'MICK', 'per':23}
+print(dict['per'])
+
+x = 23
+print(x)

d41f0e1349d0101c685cba3834d79666cae56af1
Kenean Dita <KeneanSufa@gmail.com>
2025-03-22 23:15:16 +0000

    Sorting and lambda sorting

--- DSA/Sorted.py
+++ DSA/Sorted.py
@@ -1,7 +1,11 @@
 x = [1,3,4 ,56, 2, 32, 4, 34, 2]
 print(sorted(x))
 
-tup = ("Ken", "a")
+tup = ("Ken", "Avc", "krn", "amm")
 print(sorted(tup))
 
 #comparison doesn't work with int and str
+#takes capital first and smalls in sorting them out
+
+print(sorted(tup, key=lambda k: k[1]))
+#lambda sort

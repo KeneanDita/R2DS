08e63539a5cc68e05f5379cbd62eafbffba6dcee
Kenean Dita <KeneanSufa@gmail.com>
2025-03-23 00:41:03 +0000

    Stacks using lists with a wrapper class

---
+++ DSA/stacks using list.py
@@ -0,0 +1,25 @@
+class st():
+    def __init__(self):
+        self.sta = list()
+    def push(self, item):
+        self.sta.append(item)
+    def pop(self):
+        if len(self.sta) > 0:
+            return self.sta.pop()
+        else:
+            return None
+    def peek(self):
+        if len(self.sta) > 0:
+            return self.sta[len(self.sta)-1]
+        else:
+            return None
+    def __str__(self):
+        return str(self.sta)
+
+
+stu = st()
+stu.push(2)
+stu.push("ken")
+print(stu)
+
+print(stu.peek())

2e64304de4c51a395b9808ca603a1d1cdf84c82e
Kenean Dita <KeneanSufa@gmail.com>
2025-03-24 15:28:10 +0000

    Linked list checkpoint

--- DSA/linkedlist.py
+++ DSA/linkedlist.py
@@ -1,2 +1,38 @@
-class NOde:
-    def __init__(self):
+class Node:
+    def __init__(self, d, n=None, p=None):
+        self.data = d
+        self.next_node = n
+        self.prev_node = n
+
+    def __str__(self):
+        return ('(' + str(self.data) + ')')
+
+
+class LinkedList:
+    def __int__(self, r=None):
+        self.root = r
+        self.size = 0
+
+    def add(self, d):
+        new_node = Node(d, self.root)
+        self.root = new_node
+        self.size += 1
+
+    def find (self, d):
+        this_node = self.root
+        while this_node is not None:
+            if this_node.data == d:
+                return d
+            else:
+                this_node = this_node.next_node
+        return None
+
+    def remove(self, d):
+        this_node = self.root
+        prev_node = None
+        while this_node is not None:
+            if this_node.data == d:
+                if prev_node is not None:
+                    prev_node.next_node = this_node.next_node
+                else:
+                    self.root

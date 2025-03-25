94157287f1f54a70a458e3efa6c3dca422a8ec4c
Kenean Dita <KeneanSufa@gmail.com>
2025-03-25 23:31:21 +0000

    Simple plot function demo

---
+++ R/Plot.R
@@ -0,0 +1,39 @@
+library(datasets)
+head(iris)
+
+?plot
+
+plot(iris$Species)  # Categorical variable
+plot(iris$Petal.Length)  # Quantitative variable
+plot(iris$Species, iris$Petal.Width)  # Cat x quant
+plot(iris$Petal.Length, iris$Petal.Width)  # Quant pair
+plot(iris)
+
+# Plot with options
+plot(iris$Petal.Length, iris$Petal.Width,
+  col = "#cc0000",  # Hex code for datalab.cc red
+  pch = 19,         # Use solid circles for points
+  main = "Iris: Petal Length vs. Petal Width",
+  xlab = "Petal Length",
+  ylab = "Petal Width")
+
+# PLOT FORMULAS WITH PLOT() ################################
+
+plot(cos, 0, 2*pi)
+plot(exp, 1, 5)
+plot(dnorm, -3, +3)
+
+# Formula plot with options
+plot(dnorm, -3, +3,
+  col = "#cc0000",
+  lwd = 5,
+  main = "Standard Normal Distribution",
+  xlab = "z-scores",
+  ylab = "Density")
+
+detach("package:datasets", unload = TRUE)
+
+# Clear plots
+dev.off()
+
+cat("\014")  # ctrl+L

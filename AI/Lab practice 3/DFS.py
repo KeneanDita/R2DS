a0d79ba4d99aaaeb593bb2186cacb47eb79f9e85
Kenean Dita <KeneanSufa@gmail.com>
2025-05-12 20:54:22 +0300

    Depth first algorithm

---
+++ AI/Lab practice 3/DFS.py
@@ -0,0 +1,18 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start, end=" ")
+    for next_node in graph[start] - visited:
+        dfs(graph, next_node, visited)
+    return visited
+
+graph_dfs = {
+    '0': set(['1', '2']),
+    '1': set(['0', '3', '4']),
+    '2': set(['0']),
+    '3': set(['1']),
+    '4': set(['2', '3'])
+}
+
+dfs(graph_dfs, '0')

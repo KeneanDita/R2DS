1e3d95d19acd0ae889f07f3b8d343de54e075dc6
Kenean Dita <KeneanSufa@gmail.com>
2025-07-14 21:49:58 +0300

    auto soc string generator

--- Must know concepts/Interactive mode.py
+++ Must know concepts/Interactive mode.py
@@ -1,5 +1,14 @@
 # %% 
 def sum(x,t):
+    """_summary_
+
+    Args:
+        x (_type_): _description_
+        t (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return x + t
 
 result = sum(1,2)
